97905d065ecd0dce2830511f66c48a277adfb52f
Haans Lopez <jesuslopez@ciencias.unam.mx>
2023-11-15 00:52:50 -0600

    problemas con wander seguir buscando solucion

--- Assets/Scripts/StateZombie.cs
+++ Assets/Scripts/StateZombie.cs
@@ -16,11 +16,20 @@ public class StateZombie : MonoBehaviour
     public Collider[] sensor;
     public float radio;
     public Transform playerPosition;
-    public float wanderRadius = 5.0f; // Radio del área de deambulación
     public float normalSpeed = 1.0f;  // Velocidad normal de movimiento
     public float smoothTime = 0.5f; // Tiempo de suavizado
     private Vector3 currentVelocity = Vector3.zero; // Velocidad actual de suavizado
-    private Vector3 wanderTarget;
+
+
+    public float circleDistance = 30f; // distancia entre la partícula y el círculo
+    public float angleStep = 1f; // tamaño del incremento aleatorio del ángulo
+
+    private Vector3 circlePosition;
+    private float angle;
+    private Vector3 displacement;
+
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -51,7 +60,7 @@ public class StateZombie : MonoBehaviour
             if(coll.gameObject.tag == "Player")
             {
                 playerPosition = coll.gameObject.transform;
-                Debug.Log("Posicion del jugador: " + playerPosition.position);
+                //Debug.Log("Posicion del jugador: " + playerPosition.position);
                 SetState(State.SEEK);
             }
         }
@@ -61,10 +70,31 @@ public class StateZombie : MonoBehaviour
     void UpdateWANDER()
     {
         //Logica del estado Wander
-        GetNewWanderTarget();
-        Vector3 desiredDirection = (wanderTarget - transform.position).normalized;
-        this.transform.Translate(desiredDirection * normalSpeed * Time.deltaTime);
+        Debug.Log(GetComponent<Rigidbody>().velocity.normalized);
+        // Copia la velocidad de la partícula y normalízala
+        circlePosition = GetComponent<Rigidbody>().velocity.normalized;
 
+        // Multiplica por la distancia del círculo para establecer la posición del círculo
+        circlePosition *= circleDistance;
+
+        // Incrementa el ángulo con algún valor aleatorio en cada paso
+        angle += Random.Range(0f, angleStep) - angleStep * 0.5f;
+
+        // Establece la dirección inicial
+        displacement = new Vector3(0, -1, 0);
+
+        // Multiplica por la distancia del círculo para establecer la magnitud de la dirección
+        displacement *= circleDistance;
+
+        // Rota la dirección según el ángulo de wander
+        displacement = Quaternion.Euler(0, 0, angle) * displacement;
+
+        // Calcula la posición deseada
+        Vector3 desired = circlePosition + displacement;
+
+        // Utiliza 'desired' según tus necesidades (puede ser la dirección de movimiento del NPC)
+        // Por ejemplo, podrías utilizar 'desired' para aplicar una fuerza al objeto.
+        GetComponent<Rigidbody>().AddForce(desired.normalized * normalSpeed * Time.deltaTime);
 
     }
     void UpdateSEEK()
@@ -92,11 +122,6 @@ public class StateZombie : MonoBehaviour
     {
         currentState = newState;
         // Aqui se pueden agregar acciones adicionales al cambio de estado.
-    }
-    void GetNewWanderTarget()
-    {
-        Vector2 randomCirclePoint = Random.insideUnitCircle * wanderRadius;
-        wanderTarget = new Vector3(randomCirclePoint.x, 0f, randomCirclePoint.y) + transform.position;
     }
     private void OnDrawGizmos()
     {
